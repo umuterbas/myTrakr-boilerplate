007b49f1270e7b98a56fa08f1732c9dcd877f2d1
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-07-07 11:08:47 -0700

    123

--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -21,3 +21,43 @@ class Deposit extends Transaction {
     return this.amount;
   }
 }
+
+class Transfer extends Transaction {
+  constructor(accountIdFrom, accountIdTo) {
+    super();
+    this.accountIdForm = accountIdFrom;
+    this.accountIdTo = accountIdTo
+  }
+}
+
+
+
+
+
+// will be in event listener for trans button after nicolo finishes
+$.ajax({
+  method: 'get',
+  url: 'http://localhost:3000/transactions',
+  dataType: 'json',
+}).done((data) => {
+  console.log('data get transactions ajax', data);
+  const fakeData = [{id: "123",
+  accountIdFrom: "",
+                     userName: "Umudoo" } ]
+  $.each(data, (i, transaction) => {
+    const trans = new Transaction(transaction.value);
+    $("#transaction_list").append(`
+    <tr>
+      <td>${fakeData[0].id}</td>
+      <td>${fakeData[0].userName}</td>
+      <td>${fakeData.type}</td>
+      <td>${fakeData.category}</td>
+      <td>${fakeData.description}</td>
+      <td>${fakeData.transactionAmount}</td>
+      <td>${fakeData.accountIdFrom}</td>
+      <td>${fakeData.accountIdTo}</td>
+    </tr>
+    `)
+    console.log('testingtrns', trans)
+  })
+})
