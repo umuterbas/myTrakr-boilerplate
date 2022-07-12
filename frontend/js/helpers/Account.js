2bcb8030ad41fd4d07085f98e15fdc456d0b48cf
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-07-12 11:44:27 -0700

    fix

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -11,15 +11,39 @@ class Account {
   }
 }
 
+$.ajax({
+  method: 'get',
+  url: 'http://localhost:3000/accounts',
+  dataType: 'json',
+}).done((data) => {
+  console.log('data get ajax', data);
+
+  $.each(data, (i, account) => {
+    const newAcc = new Account(account.username);
+    $("#selectuser").append(`<option>${newAcc.username}</option>`)
+    $("#from").append(`<option>${newAcc.username}</option>`)
+    $("#to").append(`<option>${newAcc.username}</option>`)
+    $("#account_summary").append(`<li>${newAcc.username}: ${newAcc.balance}$</li>`)
+    console.log('testingacc', newAcc)
+  });
+});
+
 $("#add_new_account").on('click', (event) => {
   event.preventDefault()
   if ($('#new_account_input').val() !== "") {
 
     let accountName = new Account($('#new_account_input').val());
+    $("#selectuser").append(new Option ($('#new_account_input').val()))
+    $("#from").append(new Option ($('#new_account_input').val()))
+    $("#to").append(new Option ($('#new_account_input').val()))
+    $("#account_summary").append(`<li>${'#new_account_input'}: ${newAcc.balance}$</li>`)
     console.log(accountName)
 
     // } else if (checking if the acc already exists) {
 
+  } else {
+    alert("Enter a valid value")
+    return false
   }
 
 
@@ -27,6 +51,8 @@ $("#add_new_account").on('click', (event) => {
     username: $("#new_account_input").val(),
     transactions: []
   }
+
+
 
   $.ajax({
     method: 'post',
@@ -45,29 +71,12 @@ $("#add_new_account").on('click', (event) => {
     $("#new_account_input").val("");
     alert("New account added");
 
-    $.ajax({
-      method: 'get',
-      url: 'http://localhost:3000/accounts',
-      dataType: 'json',
-    }).done((data) => {
-      console.log('data get ajax', data);
-      
-      $.each(data, (i, account) => {
-        const newAcc = new Account(account.username);
-        $("#selectuser").append(`<option>${newAcc.username}</option>`)
-        $("#from").append(`<option>${newAcc.username}</option>`)
-        $("#to").append(`<option>${newAcc.username}</option>`)
-        $("#account_summary").append(`<li>${newAcc.username}: ${newAcc.balance}$</li>`)
-        console.log('testingacc', newAcc)
-      });
-    });
+
 
   })
     .fail((error) => {
       alert(error);
     });
-
-  
 
 }
 );
--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -35,27 +35,29 @@ class Transfer extends Transaction {
 
 
 // will be in event listener for trans button after nicolo finishes
-$.ajax({
-  method: 'get',
-  url: 'http://localhost:3000/transactions',
-  dataType: 'json',
-}).done((data) => {
-  console.log('data get transactions ajax', data);
+// $.ajax({
+//   method: 'get',
+//   url: 'http://localhost:3000/transactions',
+//   dataType: 'json',
+// }).done((data) => {
+//   console.log('data get transactions ajax', data);
   
-  $.each(data, (i, transaction) => {
-    const trans = new Transaction(transaction.value);
-    $("#transaction_list").append(`
-    <tr>
-      <td>${newTransaction.accountId}</td>
-      <td>${account.userName}</td>
-      <td>${fakeData.type}</td>
-      <td>${fakeData.category}</td>
-      <td>${fakeData.description}</td>
-      <td>${fakeData.transactionAmount}</td>
-      <td>${fakeData.accountIdFrom}</td>
-      <td>${fakeData.accountIdTo}</td>
-    </tr>
-    `)
-    console.log('testingtrns', trans)
-  })
-})
+//   $.each(data, (i, transaction) => {
+//     const trans = new Transaction(transaction.value);
+//     console.log('testingtrns', trans)
+//     $("#transaction_list").append(`
+//     <tr>
+//       <td>${newTransaction.accountId}</td>
+//       <td>${account.userName}</td>
+//       <td>${fakeData.type}</td>
+//       <td>${fakeData.category}</td>
+//       <td>${fakeData.description}</td>
+//       <td>${fakeData.transactionAmount}</td>
+//       <td>${fakeData.accountIdFrom}</td>
+//       <td>${fakeData.accountIdTo}</td>
+//     </tr>
+//     `)
+//     console.log('testingtrns', trans)
+//   })
+  
+// })
