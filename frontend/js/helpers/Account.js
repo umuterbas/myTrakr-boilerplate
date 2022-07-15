8cfa6b98cba94c45a5e79511e37adc07b6832ede
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-07-14 20:39:39 -0700

    s

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -70,6 +70,54 @@ $.ajax({
   // console.log('accs', accounts)
 });
 
+
+
+$("#filter_select_id").change(function () {
+    let filterValue = $("#filter_select_id").val();
+
+    if (filterValue !== "Show All") {
+
+
+        $("#transaction_list").html("")
+        console.log(filterValue);
+        const account = getUserById(filterValue)
+        account.transactions.forEach(transaction => {
+            $("#transaction_list").append(`
+      <tr>
+        <td>${transaction.accountId}</td>
+        <td>${account.username}</td>
+        <td>${transaction.type}</td>
+        <td>${transaction.category}</td>
+        <td>${transaction.description}</td>
+        <td>${transaction.amount}</td>
+        <td>${transaction.accountIdFrom}</td>
+        <td>${transaction.accountIdTo}</td>
+      </tr>
+      `)
+        });
+    } else {
+        $.each(newTransactions, (i, transaction) => {
+        
+            $("#transaction_list").append(`
+            <tr>
+              <td>${transaction.accountId}</td>
+              <td>${newAcc.username}</td>
+              <td>${transaction.type}</td>
+              <td>${transaction.category}</td>
+              <td>${transaction.description}</td>
+              <td>${transaction.amount}</td>
+              <td>${transaction.accountIdFrom}</td>
+              <td>${transaction.accountIdTo}</td>
+            </tr>
+            `)
+        })
+    }
+
+
+});
+
+
+
 export const getUserById = function (id){
   const accountsArr = Object.values(accounts);
   return accountsArr.find(account => {
